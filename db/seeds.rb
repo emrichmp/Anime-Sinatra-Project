083b3968235900d381b34ce7c1af012b7ccfce45
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-11 09:38:30 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 emrich = User.create(name: "emrich", username: "emrichmp", password: "password")
 rahel = User.create(name: "rahel", username: "paprika", password: "yeet")
 
-Anime.create(name: "AOT", fav_character: "eren", rating: 10, user_id: emrich.id)
-Anime.create(name: "JJK", fav_character: "Sukuna", rating: 9, user_id: rahel.id)
+AOT = Anime.create(name: "AOT", fav_character: "eren", rating: 10, user_id: emrich.id)
+JJK = Anime.create(name: "JJK", fav_character: "Sukuna", rating: 9, user_id: rahel.id)
