3a764c403d1bc866f87fd20b1bf547f5617da8e5
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-17 16:35:09 -0500

    enabled sessions and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Anime_Sinatra_App"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
         erb :login
     end
 
-    get '/signup' do
+    #recieve login form and find user to log them in (create session)
+    #find and authenticate user
+    #log them in
+    #redirect user to their "home page"
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "users/#{@user.id}"
+        else
+            #tell user its invalid and redirect to /login page
+        end
+    end
 
+    get '/signup' do
+        #erb :signup
+    end
+
+    get '/users/:id' do
+        "This will be user show route"
     end
 end
