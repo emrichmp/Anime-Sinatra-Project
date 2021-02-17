5db82e5819d5d66b593fa118ede29b1b1489e97a
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-17 16:43:10 -0500

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      #true if user is logged in, otherwise false
+      !!current_user
+    end
+
+    def current_user
+      #returns current user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
 end
