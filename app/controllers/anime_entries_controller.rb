9a26e0f527be5d25bc762fee0f0391e08ddec867
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-20 15:54:06 -0500

    add new anime form

--- app/controllers/anime_entries_controller.rb
+++ app/controllers/anime_entries_controller.rb
@@ -1,7 +1,14 @@
 class AnimeEntriesController < ApplicationController
     #get anime_entries/new to render form to create animes
+    get '/anime_entries/new' do
+        erb :'/anime_entries/new'
+    end
     #post anime_entries to create a new anime
+    post '/anime_entries' do
+
+    end
     #show route for a anime entry
+
     #index route for all anime entries
     
 end
