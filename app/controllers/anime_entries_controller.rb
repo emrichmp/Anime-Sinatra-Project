e9af0a1490f57aeb23a851fbcb686014ef2d01de
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-20 16:19:32 -0500

    anime entries form

--- app/controllers/anime_entries_controller.rb
+++ app/controllers/anime_entries_controller.rb
@@ -5,9 +5,18 @@ class AnimeEntriesController < ApplicationController
     end
     #post anime_entries to create a new anime
     post '/anime_entries' do
-
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:name] != ""
+            @anime = Anime.create(name: params[:name], fav_character: params[:fav_character], rating: params[:rating], user_id: current_user.id)
+            redirect "/anime_entries/#{@anime.id}"
+        else
+            redirect '/anime_entries/new'
+        end
     end
     #show route for a anime entry
+    
 
     #index route for all anime entries
     
