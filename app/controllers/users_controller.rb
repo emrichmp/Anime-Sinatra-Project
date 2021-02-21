2e7fec7a25c854c724a94c334ff81666a055bdf9
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-02-20 21:10:37 -0500

    protects login from empty form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     #redirect user to their "home page"
     post '/login' do
         @user = User.find_by(username: params[:username])
-        if @user.authenticate(params[:password])
+        if @user.authenticate(params[:password]) && params[:username] != "" && params[:password] != ""
             session[:user_id] = @user.id
             redirect "users/#{@user.id}"
         else
